923d6e5177c78facbba61b9833f88cbb516b893f
ImJoshuaC <100665605+ImJoshuaC@users.noreply.github.com>
2022-03-03 13:27:54 +0800

    Add files via upload

---
+++ Lecture 1 Assignment/assignment_lec1_c2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+	int main (void){
+	
+	int minuend,subtrahend,answer;
+
+	minuend = 87;
+	subtrahend = 15;
+	answer = minuend - subtrahend;
+
+	printf("The final answer of subtracting %d from %d is %d", subtrahend,minuend,answer);
+
+	return 0;
+	}
